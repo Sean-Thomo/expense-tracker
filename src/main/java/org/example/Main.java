16c31d57716274e6eeceaf96feea9a8b4a843789
Siphesihle-Thomo <seansthomo@gmail.com>
2024-09-26 21:41:31 +0200

    feat: create basic logic for program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,26 @@
 package org.example;
 
+import org.json.JSONArray;
+
 public class Main {
     private static final String FILE_NAME = "expenses.json";
-//    private static final Gson GSON = new Gson();
 
     public static void main(String[] args) {
+        if (args.length == 0) return;
+
+        String action = args[0];
+        JSONArray expensesArray = new JSONArray();
+        Integer maxId = 0;
+
+        switch (action) {
+            case "add":
+                System.out.println("Adding task");
+            case "list":
+                System.out.println("Listing tasks");
+            case "summary":
+                System.out.println("Total expenses: ");
+            case "delete":
+                System.out.println("Expense deleted successfully");
+        }
     }
 }

1c5117c528acea8f48b167c7c7c95fa3d238a1c9
Siphesihle-Thomo <seansthomo@gmail.com>
2024-09-26 21:14:20 +0200

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    private static final String FILE_NAME = "expenses.json";
+//    private static final Gson GSON = new Gson();
+
+    public static void main(String[] args) {
+    }
+}

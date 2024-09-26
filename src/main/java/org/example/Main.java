a3d096423b0b7c067a0affa9c0ee6cb09de1aa6e
Siphesihle-Thomo <seansthomo@gmail.com>
2024-09-26 22:10:50 +0200

    feat: Add Expense method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,27 @@
 package org.example;
 
-import org.json.JSONArray;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     private static final String FILE_NAME = "expenses.json";
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    private static final Gson GSON = new Gson();
 
     public static void main(String[] args) {
         if (args.length == 0) return;
 
         String action = args[0];
-        JSONArray expensesArray = new JSONArray();
-        Integer maxId = 0;
+        JsonArray expensesArray = new JsonArray();
+        int maxId = 0;
+        double amount = 0;
 
         switch (action) {
             case "add":
+                addExpense(expensesArray, args[1], ++maxId, amount);
                 System.out.println("Adding task");
             case "list":
                 System.out.println("Listing tasks");
@@ -22,5 +30,17 @@ public class Main {
             case "delete":
                 System.out.println("Expense deleted successfully");
         }
+    }
+
+    private static void addExpense(JsonArray expensesArray, String taskDescription, int id, double amount) {
+        LocalDateTime now = LocalDateTime.now();
+        JsonObject newExpense = new JsonObject();
+        newExpense.addProperty("id", id);
+        newExpense.addProperty("description", taskDescription);
+        newExpense.addProperty("amount", amount);
+        newExpense.addProperty("createdAt", now.format(DATE_FORMAT));
+        newExpense.addProperty("updatedAt", now.format(DATE_FORMAT));
+        expensesArray.add(newExpense);
+        System.out.println("Task added successfully ID: " + newExpense.get("id"));
     }
 }
